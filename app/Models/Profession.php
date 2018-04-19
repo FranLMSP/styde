8fd73758bab6d398a9db659052cd03d87df2c900
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-04-19 19:33:47 -0400

    Model Attributes

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profession extends Model
 {
-	public $timestamps = false;    
+	//protected $table = 'custom_table_name';
+
+	public $timestamps = false;
+
+	protected $fillable = [
+		'title'
+	];
+
+
+	/*
+		For example:
+		If the field is boolean, the database will be tinyint(1)
+		And we can cast it to a boolean type
+	*/
+	protected $casts = [
+		'title' => 'string'
+	];
 }
