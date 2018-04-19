06972e6b8c232cfc3203fc6917f45c73749164d4
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-04-19 19:02:37 -0400

    Using eloquent models

--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Profession;
 
 class ProfessionSeeder extends Seeder
 {
@@ -12,13 +13,25 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
+        /*
+        // We can make raw queries by this way
+        DB::insert('INSERT INTO professions (title) VALEUS (:title)', [
+            'title' => "Back-end developer"
+        ]);*/
+
+        /*
         DB::table('professions')->insert([
             'title' => 'Back-end developer'
+        ]);*/
+
+        Profession::create([
+            'title' => 'Back-end developer'
         ]);
-        DB::table('professions')->insert([
+
+        Profession::create([
             'title' => 'Front-end developer'
         ]);
-        DB::table('professions')->insert([
+        Profession::create([
             'title' => 'Web designer'
         ]);
     }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\User;
+use App\Models\Profession;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -12,10 +15,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+    	// $profession = DB::select('SELECT id FROM professions WHERE title = ?', ['Back-end developer']);
+
+    	/*
+		
+		$profession = DB::table('professions')->select('id')->take(1)->get();
+		$profession->first()->id
+		
+		*/
+
+        /*
+		$profession = DB::table('professions')->select('id')->where('title', '=', 'Back-end developer  ')->first();
+
         DB::table('users')->insert([
+            'name' => 'Franco Colmenarez',
+            'email' => 'francoacg1@gmail.com',
+            'password' => bcrypt('1234'),
+            'profession_id' => $profession->id
+        ]);
+        */
+
+        User::create([
         	'name' => 'Franco Colmenarez',
         	'email' => 'francoacg1@gmail.com',
-        	'password' => bcrypt('1234')
+        	'password' => bcrypt('1234'),
+        	'profession_id' => Profession::where('title', 'Back-end developer')->value('id')
         ]);
     }
 }
